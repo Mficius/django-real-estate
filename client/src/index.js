53ddb642c414130c542ebae9433201afec792531
Mficius <mayisficius@gmail.com>
2024-05-18 16:06:47 +0100

    chore: setup nginx and redux toolkit with docker

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store'
+import './index.css';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
-	<>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</>,
-	document.getElementById("root")
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { propertiesListReducer } from './proprertyReducers';
-
-export default combineReducers({
-    propertiesList: propertiesListReducer,
-});
-

756525ce06c631c2fbcff96c4ab457833a294a47
Mficius <mayisficius@gmail.com>
2024-05-21 18:42:06 +0100

    feat: add property slice

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -9,6 +9,7 @@ class PropertySerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     country = CountryField(name_only=True)
     cover_photo = serializers.SerializerMethodField()
+    profile_photo = serializers.SerializerMethodField()
     photo1 = serializers.SerializerMethodField()
     photo2 = serializers.SerializerMethodField()
     photo3 = serializers.SerializerMethodField()
@@ -16,7 +17,7 @@ class PropertySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Property
-        fields=["id", "user", "title", "slug", "ref_code", "description", "country", "city", 
+        fields=["id", "user", "profile_photo", "title", "slug", "ref_code", "description", "country", "city", 
             "postal_code", "street_address", "property_number", "price", "tax", "final_property_price", 
             "plot_area", "total_floors", "bedrooms", "bathrooms", "advert_type", "property_type", "cover_photo",
             "photo1", "photo2", "photo3", "photo4", "published_status", "views"]
@@ -38,6 +39,9 @@ class PropertySerializer(serializers.ModelSerializer):
 
     def get_photo4(self, obj):
         return obj.photo4.url 
+
+    def get_profile_photo(self, obj):
+        return obj.user.profile.profile_photo.url
 
 
 class PropertyCreateSerializer(serializers.ModelSerializer):

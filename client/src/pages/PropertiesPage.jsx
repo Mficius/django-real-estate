fbc9e87cf4353bce569a4efa68cc2e7cf8ce8d84
Mficius <mayisficius@gmail.com>
2024-05-23 18:09:39 +0100

    feat(client): add property component

--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row';
 import { useDispatch, useSelector} from 'react-redux'
 import { toast } from "react-toastify";
 import Spinner from '../components/Spinner';
+import {toast} from 'react-toastify';
+import Property from '../components/Property';
 import { getProperties } from '../features/properties/propertySlice'
 
 const PropertiesPage = () => {
@@ -30,10 +32,22 @@ const PropertiesPage = () => {
     <>
         <Container>
             <Row>
-                <Col className='mg-top'>
-                    <h1>Properties will be displayed here</h1>
+                <Col className='mg-top text-center'>
+                    <h1>Our catalog of properties</h1>
+                    <h1 className='hr-text' />
                 </Col>
             </Row>
+            {
+              <>
+                <Row className='mt-3'>
+                  {properties.map((property)=>(
+                    <Col key={property.id} sm={12} md={6} lg={4} xl={3}>
+                      <Property property={property}/>
+                    </Col>
+                  ))}
+                </Row>
+              </>
+            }
         </Container>
     </>
   )
